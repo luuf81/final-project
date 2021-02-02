d8234d17ce310e1297ce2241a9925764ac24d365
Olof Lindahl <olindahl@gmail.com>
2021-02-02 10:36:35 +0100

    mobile mode done, starting on app menu

--- frontend/src/components/MainApp.js
+++ frontend/src/components/MainApp.js
@@ -9,8 +9,8 @@ import {
   FormControl,
   Paper,
 } from "@material-ui/core";
-import { Folder, Restore, Favorite, LocationOn } from "@material-ui/icons"
-import { makeStyles } from '@material-ui/core/styles';
+import { Folder, Restore, Favorite, LocationOn } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 import MomentUtils from "@date-io/moment";
 import {
   MuiPickersUtilsProvider,
@@ -19,7 +19,12 @@ import {
 import moment from "moment";
 
 import { user } from "../reducers/user";
-import { fetchActivities, fetchExercises, postActivity, workout } from "../reducers/workout";
+import {
+  fetchActivities,
+  fetchExercises,
+  postActivity,
+  workout,
+} from "../reducers/workout";
 //import { socketEvents } from "../reducers/user"
 import { useDispatch, useSelector } from "react-redux";
 import { fetchWorkouts } from "reducers/workout";
@@ -31,73 +36,92 @@ import Stats from "./Stats";
 
 const URL = "http://localhost:8080/users";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
-    minHeight: '800px',
-    width: '600px',
+    minHeight: "800px",
+    width: "600px",
     //padding: '0 30px',
-    margin: '30px',
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: '100%',
+    margin: "30px",
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "100%",
       margin: 0,
     },
   },
   sidePaper: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'none'
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
     },
   },
   stickyNav: {
-    position: 'fixed',
+    position: "fixed",
     bottom: 0,
-    width: '100%',
-    backgroundColor: "black"
+    width: "100%",
+    backgroundColor: "black",
   },
 }));
 
 export const MainApp = () => {
   const dispatch = useDispatch();
 
-  const classes = useStyles();  
+  const classes = useStyles();
 
   const workouts = useSelector((store) => store.workout.workouts);
   const activities = useSelector((store) => store.workout.activities);
 
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = React.useState("recents");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   useEffect(() => {
-    dispatch(fetchExercises())
+    dispatch(fetchExercises());
     dispatch(fetchWorkouts());
     dispatch(fetchActivities());
     //dispatch(socketEvents())
   }, []);
 
-
   return (
     <>
-    <Grid 
-    container
-    item
-    wrap='nowrap'
-    >
-      <Paper className={classes.paper, classes.sidePaper}><UserList /></Paper>
-      <Paper className={classes.paper}>
-        <ActivityForm />
-        <ActivityList />
-      </Paper >
-      <Paper className={classes.paper, classes.sidePaper}><Stats /></Paper>
-    </Grid>
-     <BottomNavigation value={value} onChange={handleChange} className={classes.stickyNav}>
-     <BottomNavigationAction label="Recents" value="recents" icon={<Restore />} />
-     <BottomNavigationAction label="Favorites" value="favorites" icon={<Favorite />} />
-     <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOn />} />
-     <BottomNavigationAction label="Folder" value="folder" icon={<Folder />} />
-   </BottomNavigation>
-   </>
+      <Grid container item wrap="nowrap">
+        <Paper className={(classes.paper, classes.sidePaper)}>
+          <UserList />
+        </Paper>
+        <Paper className={classes.paper}>
+          <ActivityForm />
+          <ActivityList />
+        </Paper>
+        <Paper className={(classes.paper, classes.sidePaper)}>
+          <Stats />
+        </Paper>
+      </Grid>
+      <BottomNavigation
+        value={value}
+        onChange={handleChange}
+        className={classes.stickyNav}
+      >
+        <BottomNavigationAction
+          label="Recents"
+          value="recents"
+          icon={<Restore />}
+        />
+        <BottomNavigationAction
+          label="Favorites"
+          value="favorites"
+          icon={<Favorite />}
+        />
+        <BottomNavigationAction
+          label="Nearby"
+          value="nearby"
+          icon={<LocationOn />}
+        />
+        <BottomNavigationAction
+          label="Folder"
+          value="folder"
+          icon={<Folder />}
+        />
+      </BottomNavigation>
+    </>
   );
 };
 export default MainApp;
