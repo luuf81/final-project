4f4cd4b6ebef8278caa572a30116ef0dc3511c62
Olof Lindahl <olindahl@gmail.com>
2021-02-09 23:26:10 +0100

    online users working, starting follow function

--- backend/server.js
+++ backend/server.js
@@ -215,10 +215,10 @@ app.post("/followuser", async (req, res) => {
     console.log(followedUser)
     await currentUser.followedUsers.push(followedUser)
     currentUser.save()
-    const allUsers = await User.find()
+    const allUsers = await User.find({name: req.user.name}).populate("followedUsers")
     res.status(200).json(allUsers);
   } catch (err) {
-    res.status(400).json({ message: "Could not create user", errors: err });
+    res.status(400).json({ message: "Could follow user", errors: err });
   }
 });
 
